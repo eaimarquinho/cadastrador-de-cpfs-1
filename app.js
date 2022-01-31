3d4e21a83662cc9cbdc50d99ff89e69d21c26f16
Marcos Geraldo <marcos_rg@live.com>
2022-01-31 10:56:51 -0300

    adicionando filhos a linha de comando

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const fs = require('fs');
 const nome = process.argv[2];
 const cpf = process.argv[3];
 const casado = (process.argv[4] == "1");
-
+const filhos = process.argv.slice(5);
+  
 // Passo 2: Criar um objeto com essas informações
 const pessoa = {nome, cpf, casado, filhos};
 
